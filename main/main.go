9d7e5a59a57f1296c4dc956ed571464291c6bc10
binkesi <sjtusgn@163.com>
2021-11-28 09:35:26 +0800

    Get the home page response.

--- main/main.go
+++ main/main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/gocolly/colly"
+	"github.com/PuerkitoBio/goquery"
 )
 
+func JianhangSearch() {
+	client := &http.Client{}
+	url := "http://www1.ccb.com/cn/home/map/branchSearch.html"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Host", "www1.ccb.com")
+	req.Header.Set("Proxy-Connection", "keep-alive")
+	req.Header.Set("Referer", "http://www1.ccb.com/cn/home/map/branchSearch.html")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	fmt.Println(doc.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".city_list2 j_atm_list .li_a").Each(func(i int, s *goquery.Selection) {
+		content := s.Find(".click_show").Text()
+		fmt.Printf("%d: %s\n", i, content)
+	})
+}
+
 func main() {
-	c := colly.NewCollector()
-
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	c.Visit("http://www2.ccb.com/tran/WCCMainPlatV5?CCB_IBSVersion=V5&SERVLET_NAME=WCCMainPlatV5&isAjaxRequest=true&TXCODE=NZX010&ADiv_Cd=370200&Kywd_List_Cntnt=&Enqr_MtdCd=1&PAGE=1&Cur_StCd=4")
+	JianhangSearch()
 }
