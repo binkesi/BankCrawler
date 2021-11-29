8dcbd072c1e860ff5448c0bf02b494fb98682db9
sungn2 <sungn2@lenovo.com>
2021-11-29 15:01:58 +0800

    sucess get cookie.

---
+++ banks/jianshe.go
@@ -0,0 +1,105 @@
+package banks
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func AddQuery(query *url.Values, cityNum string, getCookie bool) {
+	if !getCookie {
+		query.Set("TXCODE", "NZX010")
+		query.Add("ADiv_Cd", cityNum)
+		query.Add("Kywd_List_Cntnt", "")
+		query.Add("Enqr_MtdCd", "1")
+		query.Add("PAGE", "1")
+		query.Add("Cur_StCd", "4")
+	} else {
+		query.Add("CCB_IBSVersion", "V5")
+		query.Add("SERVLET_NAME", "WCCMainPlatV5")
+		query.Add("isAjaxRequest", "true")
+		query.Add("TXCODE", "100119")
+	}
+}
+
+func JianhangSearch(cityNum string) {
+	uri, _ := url.Parse("http://127.0.0.1:8888")
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(uri),
+		},
+	}
+	url := "http://www1.ccb.com/tran/WCCMainPlatV5"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Host", "www1.ccb.com")
+	req.Header.Set("Proxy-Connection", "keep-alive")
+	req.Header.Set("Referer", "http://www1.ccb.com/cn/home/map/branchSearch.html")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36")
+	query := req.URL.Query()
+	AddQuery(&query, cityNum, true)
+	req.URL.RawQuery = query.Encode()
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res.Body)
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		fmt.Println("goquery error", err)
+	}
+	cookie := http.Cookie{Name: "tranCCBIBS1", Value: doc.Text(), Expires: time.Now().Add(365 * 24 * time.Hour)}
+	AddQuery(&query, cityNum, false)
+	req.URL.RawQuery = query.Encode()
+	req.AddCookie(&cookie)
+	res, err = client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("decode error", err)
+	}
+	bank_list := data["OUTLET_DTL_LIST"].([]interface{})
+	for _, bank := range bank_list {
+		bank_data := bank.(map[string]interface{})
+		fmt.Println(bank_data["CCBIns_Nm"])
+		fmt.Println(bank_data["Dtl_Adr"])
+		fmt.Println(bank_data["Fix_TelNo"])
+	}
+	total_page, _ := strconv.Atoi(data["TOTAL_PAGE"].(string))
+	for i := 2; i < total_page; i++ {
+		query.Set("PAGE", strconv.Itoa(i))
+		fmt.Print(query.Encode())
+		req.URL.RawQuery = query.Encode()
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+		}
+		var data map[string]interface{}
+		err = json.NewDecoder(res.Body).Decode(&data)
+		if err != nil {
+			fmt.Println("decode error", err)
+		}
+		bank_list = data["OUTLET_DTL_LIST"].([]interface{})
+		for _, bank := range bank_list {
+			bank_data := bank.(map[string]interface{})
+			fmt.Println(bank_data["CCBIns_Nm"])
+			fmt.Println(bank_data["Dtl_Adr"])
+			fmt.Println(bank_data["Fix_TelNo"])
+		}
+	}
+}
